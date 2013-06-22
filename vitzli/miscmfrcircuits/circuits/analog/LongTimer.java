ccafa49d9b54d22cd26167887a24443bf4fc5a54
vitzli <vitzli@server.fake>
2013-06-22 23:41:28 +0600

    Long Timer added

--- vitzli/miscmfrcircuits/circuits/analog/LongTimer.java
+++ vitzli/miscmfrcircuits/circuits/analog/LongTimer.java
@@ -1,7 +1,28 @@
 package vitzli.miscmfrcircuits.circuits.analog;
 
+import net.minecraft.nbt.NBTTagCompound;
 import powercrystals.minefactoryreloaded.api.rednet.IRedNetLogicCircuit;
 import vitzli.miscmfrcircuits.lib.ValueFunctions;
+
+/*
+ * Inputs:
+ * Ts 	- 1..255 use square wave generator for shorter periods of time
+ * 			 255 is a bit uncommon, but you can set 150 seconds in one field 
+ * Tm 	- 0..255 - minute setting
+ * Th 	- 0..24 - hour setting
+ * DC 	- Duty Cycle; 0 is for 1 tick width, 1..100%
+ * EN# 	- send activate signal and it will stop counting for 
+ * 		the duration of given signal, it will continue from the moment it was interrupted 
+ * RST 	- Reset the timer.
+ * 
+ * Outputs:
+ * Q 	- stays active during activePeriod
+ * Qs 	- activates every for one tick second of activePeriod
+ * Qm 	- one tick every minute
+ * Qh 	- one tick every hour
+ *   
+ * 
+ */ 
 
 public class LongTimer implements IRedNetLogicCircuit {
 
@@ -9,19 +30,22 @@ public class LongTimer implements IRedNetLogicCircuit {
 			"EN#", "RST" };
 	private String[] outputPinNames = new String[] { "Q", "Qs", "Qm", "Qh" };
 
-	private int Ts = 0, Tm = 0, Th = 0, duty_cycle = 1;
-	private int period = 1;
-	private int activePeriod = 0;
-	private int currentShift = 0;
-	private boolean state = true;
-	private boolean timerDisabled = false;
+	private int Ts = 0, // values from interface, 1..255
+			Tm = 0, // 0..255
+			Th = 0, // 0.. 24
+			dutyCycle = 50; // 50%
+	
+	private int period = 1; // total pulse period T
+	private int activePeriod = 0; // duty period D.T
+	private int currentShift = 0; // ticks from last rising edge of current period
+	
+	private boolean state = true; // timer state
+	private boolean timerDisabled = false; // disabled via interface
 
-	private long pickupMoment = 0;
+	private long startMoment = 0; // worldTime of the last rising edge
 
-	private int currTs, currTm, currTh, currDC;
-	private boolean reset;
-
-	private int[] result = new int[4];
+	private int currTs, currTm, currTh, currDC; // internal cycle variables
+	private boolean reset; // reset timer
 
 	@Override
 	public int getInputCount() {
@@ -37,7 +61,8 @@ public class LongTimer implements IRedNetLogicCircuit {
 	public int[] recalculateOutputValues(long worldTime, int[] inputValues) {
 		timerDisabled = inputValues[4] != 0 ? true : false;
 		if (timerDisabled) {
-			state = false;
+			// don't change the state !!!
+			startMoment++; // shift beginning when timer is disabled 
 			return new int[] { 0, 0, 0, 0 };
 		}
 
@@ -48,39 +73,37 @@ public class LongTimer implements IRedNetLogicCircuit {
 		currDC = ValueFunctions.ConstraintInt(inputValues[3], 0, 100);
 
 		if (currTs != Ts || currTm != Tm || currTh != Th
-				|| currDC != duty_cycle) {
+				|| currDC != dutyCycle) {
 			changeParameters();
 		}
 
 		if (reset) {
 			changeParameters();
 			state = true;
-			pickupMoment = worldTime;
+			startMoment = worldTime;
 		}
 		
-		currentShift = (int) (worldTime - pickupMoment); 
+		currentShift = (int) (worldTime - startMoment); 
 		state = currentShift < activePeriod;
 		
 		if (currentShift >= period) {
 			// here goes new pulse
 			state = true;
-			pickupMoment = worldTime;
-			currentShift = (int) (worldTime - pickupMoment);
+			startMoment = worldTime;
+			currentShift = (int) (worldTime - startMoment);
 		}
 		
-		result[0] = state ? 15 : 0;
-		result[1] = (currentShift % 20 == 0 && state) ? 15 : 0; // 1 second
-		result[2] = (currentShift % 1200 == 0 && state) ? 15 : 0; // 1 minute
-		result[3] = (currentShift % 72000 == 0 && state) ? 15 : 0; // 1 hour
-
-		return new int[] { result[0], result[1], result[2], result[3] };
+		return new int[] { state ? 15 : 0,
+				(currentShift % 20 == 0 && state) ? 15 : 0,
+				(currentShift % 1200 == 0 && state) ? 15 : 0,
+				(currentShift % 72000 == 0 && state) ? 15 : 0 };
 	}
 
 	private void changeParameters() {
 		Ts = currTs;
 		Tm = currTm;
 		Th = currTh;
-		duty_cycle = currDC;
+		dutyCycle = currDC;
 
 		period = 20 * (Ts + Tm * 60 + Th * 60);
 		activePeriod = ((int) Math.round((float) period
@@ -90,20 +113,41 @@ public class LongTimer implements IRedNetLogicCircuit {
 
 	@Override
 	public String getUnlocalizedName() {
-		// TODO Auto-generated method stub
-		return "miscIC.analog.longtimer";
+		return "miscIC.analog.looongtimer";
 	}
 
 	@Override
 	public String getInputPinLabel(int pin) {
-		// TODO Auto-generated method stub
 		return inputPinNames[pin];
 	}
 
 	@Override
 	public String getOutputPinLabel(int pin) {
-		// TODO Auto-generated method stub
 		return outputPinNames[pin];
+	}
+
+	@Override
+	public void readFromNBT(NBTTagCompound tag) {
+		state = tag.getBoolean("state");
+		currTs = tag.getInteger("Ts");
+		currTm = tag.getInteger("Tm");
+		currTh = tag.getInteger("Th");
+		currDC = tag.getInteger("dutyCycle");
+		timerDisabled = tag.getBoolean("timerDisabled");
+		startMoment = tag.getLong("startMoment");
+		changeParameters();
+	}
+
+	@Override
+	public void writeToNBT(NBTTagCompound tag) {
+		tag.setBoolean("state", state);
+		tag.setInteger("Ts", Ts);
+		tag.setInteger("Tm", Tm);
+		tag.setInteger("Th", Th);
+		tag.setInteger("dutyCycle", dutyCycle);
+		tag.setBoolean("timerDisabled", timerDisabled);
+		tag.setLong("startMoment", startMoment);
+		
 	}
 
 }
